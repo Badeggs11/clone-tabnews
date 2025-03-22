0397d88c4c05a9574c33a70ea6330a5407bb2e62
Badeggs11 <84923646+Badeggs11@users.noreply.github.com>
2025-03-22 22:22:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 function Home() {
-    return <h1> Nao devemos remoer pequenas derrotas quando temos tanto a agradecer
-         </h1>
+  return (
+    <h1>
+      {" "}
+      Nao devemos remoer pequenas derrotas quando temos tanto a agradecer
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identacao errada");
 }
 
 export default Home;
