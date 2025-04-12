c8b99ad591a177d1dcad18e5352ab6814e42e8df
Badeggs11 <84923646+Badeggs11@users.noreply.github.com>
2025-04-12 22:05:31 +0000

    add `database.js` e `.env`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,4 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch(
-    "https://turbo-fiesta-v7v6rpv44vgh64g7-3000.app.github.dev/api/v1/status",
-  );
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
